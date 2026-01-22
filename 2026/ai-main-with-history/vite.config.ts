80ba005bc2780040c19e153a8bf9fd06dfb831b6
Anna <eppelas@gmail.com>
2026-01-22 00:21:36 +0000

    Add base path for GitHub Pages

--- 2026/ai-main-with-history/vite.config.ts
+++ 2026/ai-main-with-history/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: mode === 'production' ? '/aim-annual-report-2025/' : '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
